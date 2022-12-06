4caffdaab214d5a2a1777bf62da7a8fe09815f4c
SerDes010 <sdesyatoff@gmail.com>
2022-12-06 21:31:22 +0300

    финал

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,85 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping()
+public class AdminController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping("admin")
+    public String pageForAdmin(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping("admin/{id}")
+    public String getUser(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "user";
+    }
+
+    @GetMapping("admin/new")
+    public String pageCreateUser(@ModelAttribute("user") User user, Model model) {
+        model.addAttribute("listRoles",roleService.getAllRoles());
+        return "create";
+    }
+
+    @PostMapping("admin/new")
+    public String pageCreate(@ModelAttribute("user")
+                             @Valid User user, BindingResult bindingResult,
+                             @RequestParam("listRoles") ArrayList<Long> roles) {
+        if (bindingResult.hasErrors()) {
+            return "create";
+        }
+        if (userService.getUserByLogin(user.getUsername()) != null) {
+            bindingResult.addError(new FieldError("username", "username",
+                    String.format("User with name \"%s\" is already exist!", user.getUsername())));
+            return "create";
+        }
+        user.setRoles(roleService.findByIdRoles(roles));
+        userService.addUser(user);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/admin/delete/{id}")
+    public String pageDelete(@PathVariable("id") long id) {
+        userService.removeUser(id);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("admin/edit/{id}")
+    public String pageEditUser(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user",userService.getUserById(id));
+        model.addAttribute("listRoles", roleService.getAllRoles());
+        return "edit";
+    }
+
+    @PostMapping("admin/edit")
+    public String pageEdit(@Valid User user, BindingResult bindingResult,
+                           @RequestParam("listRoles") ArrayList<Long>roles) {
+        if (bindingResult.hasErrors()) {
+            return "edit";
+        }
+        user.setRoles(roleService.findByIdRoles(roles));
+        userService.updateUser(user);
+        return "redirect:/admin";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -0,0 +1,76 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.DAO.UserDAO;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserDAO userDAO;
+    private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO, RoleService roleService, PasswordEncoder passwordEncoder) {
+        this.userDAO = userDAO;
+        this.roleService = roleService;
+        this.passwordEncoder = passwordEncoder;
+        addDefaultUser();
+    }
+
+    @Override
+    public User passwordCoder(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return user;
+    }
+    @Override
+    public List<User> getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+
+    @Override
+    public User getUserById(long id) {
+        return userDAO.getUserById(id);
+    }
+
+    @Override
+    public void addUser(User user) {
+        userDAO.addUser(passwordCoder(user));
+    }
+
+    @Override
+    public void removeUser(long id) {
+        userDAO.removeUser(id);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userDAO.updateUser(passwordCoder(user));
+    }
+
+    @Override
+    public User getUserByLogin(String username) {
+       return userDAO.getUserByLogin(username);
+    }
+
+    @Override
+    public void addDefaultUser() {
+            Set<Role> roleSet = new HashSet<>();
+            roleSet.add(roleService.findById(1L));
+            Set<Role> roleSet2 = new HashSet<>();
+            roleSet2.add(roleService.findById(1L));
+            roleSet2.add(roleService.findById(2L));
+            User user1 = new User("Gamlet", "Prince", (byte) 33, "user@mail.ru", "user", "user", roleSet);
+            User user2 = new User("Vasiya", "Cool", (byte) 33, "vasiya@mail.ru", "admin", "admin", roleSet2);
+            addUser(user1);
+            addUser(user2);
+    }
+}
